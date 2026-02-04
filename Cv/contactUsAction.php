8cf20f4facff6ea49b27ea6219feefb8f1bd9d71
Jason Mrad <jaymrad222@gmail.com>
2026-02-04 12:40:15 +0200

    Global refactoring and add sports website.

---
+++ Cv/contactUsAction.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<!--This is just for code because FREE github doesnt support PHP.-->
+        Thank You! Your message was submitted.<br><br>
+        Name: <?php echo($_POST["name"]);?>.<br>
+        Email: <?php echo($_POST["email"]);?>.<br>
+        Message: <?php echo($_POST["message"]);?><br><br>
+        <a href="/cv.html">Return to Cv Page.</a>
+
+
+    
+</body>
+</html>
